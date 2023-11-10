1064a68aa68776e00fa62642e2c8b2126c517eb0
juliecreationsweb <julieandrecreationsweb@gmail.com>
2023-11-10 09:38:11 +0100

    MAJ NAVBAR/BOUTTONS -/+SCRIPT/css

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php 
 
-function add_admin_link_to_nav_menu($items, $args) {
+function planty_add_admin_link_to_nav_menu($items, $args) {
     // Je vérifie si l'utilisateur est connecté
     if (is_user_logged_in()) {
         // Je vérifie si l'utilisateur actuel a le rôle d'administrateur
         $user = wp_get_current_user();
         if (in_array('administrator', $user->roles)) {
             // J' Ajoute le lien "admin" au menu
-            $admin_link = '<li><a href="/wp-admin/">Admin</a></li>';
+            $admin_link = '<li class="admin-link"><a class="Admin-button" href="/wp-admin/">Admin</a></li>';
 
             // Trouve le milieu de la barre de navigation
             $items_array = explode('</li>', $items);
@@ -23,14 +23,14 @@ function add_admin_link_to_nav_menu($items, $args) {
     return $items;
 }
 
-add_filter('wp_nav_menu_items', 'add_admin_link_to_nav_menu', 10, 2);
+add_filter('wp_nav_menu_items', 'planty_add_admin_link_to_nav_menu', 10, 2);
 
 
 
-function enqueue_custom_script() {
-    wp_enqueue_script('custom-script', get_template_directory_uri() . '/script.js', array('jquery'), '1.0', true);
+function planty_enqueue_custom_script() {
+    wp_enqueue_script('planty-script', get_stylesheet_directory_uri() . '/script.js', array('jquery'), '1.0', true);
 }
-add_action('wp_enqueue_scripts', 'enqueue_custom_script');
+add_action('wp_enqueue_scripts', 'planty_enqueue_custom_script');
 
 ?>
 
--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
-    $('.minus').click(function () {
-        var $input = $(this).parent().find('input');
-        var count = parseInt($input.val()) - 1;
+jQuery(document).ready(function() {
+    console.log("toto")
+    jQuery('.minus').click(function () {
+        var jQueryinput = jQuery(this).parent().find('input');
+        var count = parseInt(jQueryinput.val()) - 1;
         count = count < 0 ? 0 : count; // Garantit que la valeur ne descend pas en dessous de zéro
-        $input.val(count);
-        $input.change();
+        jQueryinput.val(count);
+        jQueryinput.change();
         return false;
     });
-    $('.plus').click(function () {
-        var $input = $(this).parent().find('input');
-        $input.val(parseInt($input.val()) + 1);
-        $input.change();
+    jQuery('.plus').click(function () {
+        var jQueryinput = jQuery(this).parent().find('input');
+        jQueryinput.val(parseInt(jQueryinput.val()) + 1);
+        jQueryinput.change();
         return false;
     });
 });
